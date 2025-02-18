0183e293deded35a080d899d6a5be6d385c7f3d9
jcacuna <jcacunainge@gmail.com>
2025-02-18 09:14:35 -0500

    ref[update proyecto]

--- www.barberia.com/frontend/vite.config.js
+++ www.barberia.com/frontend/vite.config.js
@@ -8,7 +8,12 @@ export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
-})
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+  server: {
+    host: "0.0.0.0", // Permite accesos externos
+    port: process.env.PORT || 5173, // Usa el puerto de Render
+    strictPort: true, // Evita cambiar de puerto automáticamente
+  },
+});
